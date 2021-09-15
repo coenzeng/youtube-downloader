42f8847abcc3a81af8aec695a96fd3cff3d75642
Coen Zeng <czeng@coveo.com>
2021-09-15 17:14:10 -0400

    first commit: setup vue, vite, router

---
+++ src/router/router.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+
+]
+
+const router = createRouter({
+  history: createWebHistory(), //use web history API in browser go allow going forward and backwards
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+home
+<p ref="p">My name is {{ name }} and my age is {{ age }}</p>
+<button @click="handleClick">Click Me</button>
+<input type="text" v-model="name">
+<button @click="stopEffect1">Stop Watching</button>
+  </div>
+</template>
+
+<script>
+import { computed, ref } from '@vue/reactivity'
+import { watchEffect } from '@vue/runtime-core'
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  setup(){//this runs before any lifecycle hook
+    
+    const p = ref(null)
+    const name = ref("Coenwerew")
+    const age = ref(40)
+
+    const handleClick = () => {
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      name.value = "bro"
+    }
+/*
+    const name1 = computed(() => {
+
+    })
+
+    */
+   const stopEffect = watchEffect(() => {
+     console.log(name.value)
+   })
+
+  const stopEffect1 = () => {
+     stopEffect()
+   }
+    return { name, age, handleClick, p, stopEffect1 } //same as { name: name, age: age }
+  }
+}
+</script>
